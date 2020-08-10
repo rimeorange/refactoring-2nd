fdf320cb29753433bfc3f66b444f66669da426b3
rimeorange <rimeorange83@gmail.com>
2020-08-11 01:20:39 +0900

    임시 변수를 질의 함수로 바꾸기

--- src/chp1/index.js
+++ src/chp1/index.js
@@ -33,32 +33,39 @@ function statement(invoice, plays) {
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
-}
 
-function amountFor(aPerformance, play) {
-    let result = 0;
-
-    switch (play.type) {
-        case "tragedy": //비극
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy": //희극
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 1000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
+    function playFor(perf) {
+        return plays[perf.playID];
     }
 
-    return result;
+    function amountFor(aPerformance, play) {
+        let result = 0;
+
+        switch (play.type) {
+            case "tragedy": //비극
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy": //희극
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 1000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${play.type}`);
+        }
+
+        return result;
+
+    }
 
 }
+
+
 
 
 console.log(statement(INVOICE[0], PLAY));
