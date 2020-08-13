a54173b191dec5480d8dc43cec387de95d02b3e8
rimeorange <rimeorange83@gmail.com>
2020-08-13 23:54:01 +0900

    반복문 쪼개기 volumeCredits 값 따로 빼기

--- src/chp1/index.js
+++ src/chp1/index.js
@@ -13,12 +13,15 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
 
-        volumeCredits += volumeCreditsFor(perf);
-
         // 청구 내역을 출력한다.
         result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
+
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+
     result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
