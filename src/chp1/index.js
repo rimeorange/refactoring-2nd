e19f4447438f1e982f8bb70f5f9c65ea4a2922e6
rimeorange <rimeorange83@gmail.com>
2020-08-18 16:40:50 +0900

    얄은 복사 수행 적용.

--- src/chp1/index.js
+++ src/chp1/index.js
@@ -9,8 +9,13 @@ function playFor(aPerformance) {
 function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance);
     return renderPlainText(statementData, plays);
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        return result;
+    }
 }
 
 function renderPlainText(data, plays) {
