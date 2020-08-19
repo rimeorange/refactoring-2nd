3f969a4ba3e3d7f6f2f81fdb6980a1338577c240
rimeorange <rimeorange83@gmail.com>
2020-08-19 17:51:17 +0900

    코드 분리.

--- src/chp1/createStatementData.js
+++ src/chp1/createStatementData.js
@@ -1,18 +1,10 @@
-import INVOICE from "../invoices.json";
-import PLAY from "../plays.json";
-
-
-function playFor(aPerformance) {
-    return play[aPerformance.playID];
-}
-
-function statement(invoice, plays) {
+export default function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
     statementData.totalAmount = totalAmount(statementData);
     statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlainText(statementData, plays);
+    return statementData;
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
@@ -67,29 +59,6 @@ function statement(invoice, plays) {
         return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
     }
 }
-
-function renderPlainText(data, plays) {
-    let result = `청구 내역(고객명: ${data.customer})\n`;
-
-    for (let perf of data.performances) {
-        // 청구 내역을 출력한다.
-        result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-    }
-
-    result += `총액: ${usd(data.totalAmount)}\n`;
-    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-    return result;
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-            {
-                style: "currency",
-                currency: "USD",
-                minimumFractionDigits: 2
-            }).format(aNumber / 100);
-    }
-}
-
 
 
 
---
+++ src/chp1/statement.js
@@ -0,0 +1,52 @@
+import INVOICE from "../invoices.json";
+import PLAYS from "../plays.json";
+import createStatementData from './createStatementData.js'
+
+    function statement(invoice, plays) {
+      return renderPlainText(createStatementData(invoice, plays));
+    }
+
+    function renderPlainText(data, plays) {
+        let result = `청구 내역(고객명: ${data.customer})\n`;
+
+        for (let perf of data.performances) {
+            // 청구 내역을 출력한다.
+            result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+        }
+
+        result += `총액: ${usd(data.totalAmount)}\n`;
+        result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+        return result;
+    }
+
+    function htmlStatement(invoice, plays) {
+        return renderHtml(createStatementData(invoice, plays));
+    }
+
+    function renderHtml(data) {
+
+        let result = `<h1>청구 내역(고객명: ${data.customer})</h1>\n`;
+          result += `<table>\n`;
+          result += `<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>`;
+          for (let perf of data.performances) {
+            // 청구 내역을 출력한다.
+            result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+            result += `<td>${usd(perf.amount)}</td></tr>\n`;
+          }
+          result += `</table>\n`;
+          result += `총액: ${usd(data.totalAmount)}\n`;
+          result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+          return result;
+        }
+    }
+
+    function usd(aNumber) {
+      return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2
+      }).format(aNumber / 100);
+
+
+      document.getElementById("app2").innerText = statement(INVOICE[0], PLAYS);
+      document.getElementById("app").innerHTML = htmlStatement(INVOICE[0], PLAYS);
