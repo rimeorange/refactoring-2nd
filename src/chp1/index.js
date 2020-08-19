c87b43ed34d0ad341ec28296fab2fa29ded84b64
rimeorange <rimeorange83@gmail.com>
2020-08-19 16:43:13 +0900

    totalAmount, totalVolumeCredits 함수 이동..

--- src/chp1/index.js
+++ src/chp1/index.js
@@ -10,12 +10,15 @@ function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData, plays);
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -43,47 +46,7 @@ function statement(invoice, plays) {
             default:
                 throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
         }
-
         return result;
-
-    }
-}
-
-function renderPlainText(data, plays) {
-    let result = `청구 내역(고객명: ${data.customer})\n`;
-
-    for (let perf of data.performances) {
-        // 청구 내역을 출력한다.
-        result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-    }
-
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-    return result;
-
-    function totalAmount() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
-    }
-
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
-        }
-        return result;
-    }
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-            {
-                style: "currency",
-                currency: "USD",
-                minimumFractionDigits: 2
-            }).format(aNumber / 100);
     }
 
     function volumeCreditsFor(aPerformance) {
@@ -96,6 +59,43 @@ function renderPlainText(data, plays) {
         return result;
     }
 
+    function totalAmount(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.amount;
+        }
+        return result;
+    }
+
+    function totalVolumeCredits(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    }
+}
+
+function renderPlainText(data, plays) {
+    let result = `청구 내역(고객명: ${data.customer})\n`;
+
+    for (let perf of data.performances) {
+        // 청구 내역을 출력한다.
+        result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    return result;
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency",
+                currency: "USD",
+                minimumFractionDigits: 2
+            }).format(aNumber / 100);
+    }
 }
 
 
