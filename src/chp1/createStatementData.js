d1a87a761606a5a76950210a13a8633260dc41a8
rimeorange <rimeorange83@gmail.com>
2020-08-21 14:15:26 +0900

    함수들을 계산기로 옴기기

--- src/chp1/createStatementData.js
+++ src/chp1/createStatementData.js
@@ -3,6 +3,29 @@ class PerformanceCalculator {
         this.performance = aPerformance;
         this.play = aPlay;
     }
+
+    get amount() {
+        let result = 0;
+
+        switch (this.play.type) {
+            case "tragedy": //비극
+                result = 40000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case "comedy": //희극
+                result = 30000;
+                if (this.performance.audience > 20) {
+                    result += 1000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${this.play.type}`);
+        }
+        return result;
+    }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -17,8 +40,8 @@ export default function createStatementData(invoice, plays) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.amount = calculator.amount
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
@@ -27,34 +50,15 @@ export default function createStatementData(invoice, plays) {
     }
 
     function amountFor(aPerformance) {
-        let result = 0;
-
-        switch (aPerformance.play.type) {
-            case "tragedy": //비극
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy": //희극
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 1000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-        }
-        return result;
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
     }
 
     function volumeCreditsFor(aPerformance) {
         let result = 0;
         // 포인트를 적립한다.
-        result += Math.max(aPerformance.audience - 30, 0);
+        result += Math.max(this.performance.audience - 30, 0);
         // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+        if ("comedy" === aPerformance.play.type) result += Math.floor(this.performance.audience / 5);
 
         return result;
     }
