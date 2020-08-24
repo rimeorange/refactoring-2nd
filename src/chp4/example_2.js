eef026662dc8f2f77d4314379742e62e915e8315
rimeorange <rimeorange83@gmail.com>
2020-08-24 15:50:14 +0900

    cha4 ex2 - 변수 교체

--- src/chp4/example_2.js
+++ src/chp4/example_2.js
@@ -2,7 +2,7 @@
 function price(order) {
     // 가격(price) = 기본가격 - 수량 할인 + 배송비
     const basePrice = order.quantity * order.itemPrice;
-    return order.quantity * order.itemPrice -
+    return basePrice -
         Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-        Math.min(order.quantity * order.itemPrice * 0.1, 100);
+        Math.min(basePrice * 0.1, 100);
 }
