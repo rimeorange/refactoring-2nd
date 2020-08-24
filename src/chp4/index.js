8149ed0dfe5d5224a3db0e7db152809d556e78f9
rimeorange <rimeorange83@gmail.com>
2020-08-24 14:24:48 +0900

    cha4 - 세부사항 출력코드 지역 변수 두 개를 매개 변수로 받는 함수로 추출

--- src/chp4/index.js
+++ src/chp4/index.js
@@ -13,19 +13,19 @@ function printOwing(invoice) {
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
     // 세부 사항을 출력한다.
-    printDetails();
+    printDetails(invoice, outstatnding);
 
-    function printDetails() {
-        console.log('고객명 : ${invoice.customer}');
-        console.log('채무액 : ${outstanding}');
-        console.log('마감일 : ${invoice.dueDate.toLocalDateString()}');
-    }
 }
-
 function printBanner() {
     console.log("****************");
     console.log("**** 고객 채무 ****")
     console.log("****************");
+}
+
+function printDetails(invoice, outstatnding) {
+    console.log('고객명 : ${invoice.customer}');
+    console.log('채무액 : ${outstanding}');
+    console.log('마감일 : ${invoice.dueDate.toLocalDateString()}');
 }
 
 
