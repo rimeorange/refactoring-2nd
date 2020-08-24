0f07d4c7606f974dcb6f246d8f7a900d4833776b
rimeorange <rimeorange83@gmail.com>
2020-08-24 14:18:56 +0900

    cha4 배너 출력 로직, 세부사항 출력 로직 추출

--- src/chp4/index.js
+++ src/chp4/index.js
@@ -1,11 +1,7 @@
-
-
 function printOwing(invoice) {
     let outstatnding = 0;
 
-    console.log("****************");
-    console.log("**** 고객 채무 ****")
-    console.log("****************");
+    printBanner();
 
     // 미해결 채무(outstanding)를 계산한다.
     for (const o of invoice.orders) {
@@ -17,7 +13,19 @@ function printOwing(invoice) {
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
     // 세부 사항을 출력한다.
-    console.log('고객명 : ${invoice.customer}');
-    console.log('채무액 : ${outstanding}');
-    console.log('마감일 : ${invoice.dueDate.toLocalDateString()}');
+    printDetails();
+
+    function printDetails() {
+        console.log('고객명 : ${invoice.customer}');
+        console.log('채무액 : ${outstanding}');
+        console.log('마감일 : ${invoice.dueDate.toLocalDateString()}');
+    }
 }
+
+function printBanner() {
+    console.log("****************");
+    console.log("**** 고객 채무 ****")
+    console.log("****************");
+}
+
+
