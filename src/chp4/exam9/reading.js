666eb0089454b71dcc9ff7f0f5e13cb0edbe50a6
rimeorange <rimeorange83@gmail.com>
2020-09-02 10:34:11 +0900

    cha4 ex8 - 레코드 캡슐화 적용.

--- src/chp4/exam9/reading.js
+++ src/chp4/exam9/reading.js
@@ -1,1 +1,27 @@
+class Reading
+{
+    constructor(data) {
+        this._customer = data.customer;
+        this._quantity = data.quantity;
+        this._month = data.month;
+        this._year = data.year;
+        this._data = data;
+    }
+
+    get customer() {
+        return this._customer;
+    }
+
+    get quantity() {
+        return this._quantity;
+    }
+
+    get month() {
+        return this._month;
+    }
+
+    get year() {
+        return this._year;
+    }
+}
 reading = {customer:"ivan", quantity: 10, month:5, year:2017};
