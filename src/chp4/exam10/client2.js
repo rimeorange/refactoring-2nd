fc45e941b70f286988c92e5721803a3edae27d6d
rimeorange <rimeorange83@gmail.com>
2020-09-02 11:21:27 +0900

    cha4 ex10 - client2 수정.

--- src/chp4/exam10/client2.js
+++ src/chp4/exam10/client2.js
@@ -1,5 +1,5 @@
-const aReading = aquireReading();
-const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
-const taxableCharge = Math.max(0, base - taxThreshjold(aReading.year));
+const rawReading = aquireReading();
+const aReading = enrichReading(rawReading);
+const taxableCharge = aReading.taxableCharge;
 
 
--- src/chp4/exam10/reading.js
+++ src/chp4/exam10/reading.js
@@ -3,5 +3,6 @@ const reading = {customer:"ivan", quantity: 10, month:5, year:2017};
 function enrichReading(original) {
     const result = _.cloneDeep(original);
     result.baseCharge = calulateBaseCharge(result);
+    result.taxableCharge = Math.max(0, result.baseCharge - taxThreshjold(result.year));
     return result;
 }
