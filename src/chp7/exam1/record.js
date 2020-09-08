4b75898abae06da97c4c97ca7fb26117284b689e
rimeorange <rimeorange83@gmail.com>
2020-09-08 14:40:53 +0900

    cha7.1 - 객체로 만듬.

--- src/chp7/exam1/record.js
+++ src/chp7/exam1/record.js
@@ -1,5 +1,15 @@
+class Organization {
+    constructor(data) {
+        this._data = data;
+    }
+}
+
 const organization= {name:"애크미 구스베리", country:"GB"};
 
 function getRawDataOfOrganization(){
+    return organization._data;
+}
+
+function getOrganization(){
     return organization;
 }
