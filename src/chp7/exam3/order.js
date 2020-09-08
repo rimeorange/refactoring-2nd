e7a9262c575b002fff93306d6f2abd75e339a075
rimeorange <rimeorange83@gmail.com>
2020-09-08 16:34:33 +0900

    cha7.3 - 변수 캡슐화

--- src/chp7/exam3/order.js
+++ src/chp7/exam3/order.js
@@ -1,5 +1,14 @@
 class Order {
     constructor(data) {
-        this.priority = data.priority;
+        this._priority = data._priority;
+    }
+
+
+    get priority() {
+        return this._priority;
+    }
+
+    set priority(aString) {
+        this._priority = aString;
     }
 }
