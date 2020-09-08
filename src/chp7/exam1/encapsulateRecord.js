70a60e8d9c031d1ded17e80d3949dbb4c1e5aa93
rimeorange <rimeorange83@gmail.com>
2020-09-08 14:47:17 +0900

    cha7.1 - 세터를 사용하도록 변경.

--- src/chp7/exam1/encapsulateRecord.js
+++ src/chp7/exam1/encapsulateRecord.js
@@ -7,13 +7,13 @@ class Oranization {
     get name() {
         return this._name;
     }
-    set name(value) {
-        this._name = value;
+    set name(aString) {
+        this._name = aString;
     }
     get country() {
         return this._country;
     }
-    set country(value) {
-        this._country = value;
+    set country(aCountryCode) {
+        this._country = aCountryCode;
     }
 }
