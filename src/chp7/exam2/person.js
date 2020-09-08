4878b368ac5c35b1320be247e54bcdeab81b2503
rimeorange <rimeorange83@gmail.com>
2020-09-08 16:09:25 +0900

    cha7.2 - 캡술화 위한 메서드 추가.

--- src/chp7/exam2/person.js
+++ src/chp7/exam2/person.js
@@ -13,4 +13,14 @@ class Person {
     set courses(asList) {
         this._courses = asList;
     }
+
+    addCourse(aCourse) {
+        this._courses.push(acourse);
+    }
+
+    removeCourse(aCourse, fnIfAbsent = () => {thrownew RangeError();}) {
+        const index = this._courses.indexOf(aCourse);
+        if (index === -1) fnIfAbsent();
+        else this._courses.splice(index, 1);
+    }
 }
