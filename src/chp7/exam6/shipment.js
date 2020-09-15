dc375a3e17d566e8b24001e057063360144c70e6
rimeorange <rimeorange83@gmail.com>
2020-09-15 16:25:10 +0900

    cha7.6 - 필드 옴기기및 TrackingInformation 클래스 삭제

--- src/chp7/exam6/shipment.js
+++ src/chp7/exam6/shipment.js
@@ -2,12 +2,8 @@ class Shipment {
     get trackingInfo() {
         return '${this.shippingCompany}: ${this.trackingNumber}';
     }
-    get trackingInformation()   {return this,_trackingInformation;}
-    set trackingInformation(aTrackingInformaion) {
-        this._trackingInformation = aTrackingInformaion;
-    }
-
     get shippingCompany()   {return this,_shippingCompany;} //배송 회사
     set shippingCompany(arg) {this.shippingCompany = arg;}
-
+    get trackingNumber()    {return this._trackingNumber;}  //추적 번호
+    set trackingNumber(arg) {this._trackingNumber = arg;}
 }
--- src/chp7/exam6/trackingInformation.js
+++ src/chp7/exam6/trackingInformation.js
@@ -1,6 +1,5 @@
+//삭제
 class TrackingInformation {
-    get trackingNumber()    {return this._trackingNumber;}  //추적 번호
-    set trackingNumber(arg) {this._trackingNumber = arg;}
     get display() {
         return '${this.shippingCompany}: ${this.trackingNumber}';
     }
