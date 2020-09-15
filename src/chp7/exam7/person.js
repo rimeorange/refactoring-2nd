46613b55d2a5be6fca1386a4e2a01adc271d81b9
rimeorange <rimeorange83@gmail.com>
2020-09-15 16:52:39 +0900

    cha7.7 - init commit

---
+++ src/chp7/exam7/person.js
@@ -0,0 +1,9 @@
+class Person {
+    constructor(name) {
+        this._name = name;
+    }
+
+    get name()  {return this._name;}
+    get department()    {return this._department;}
+    set department(arg)    {this._department = arg;}
+}
